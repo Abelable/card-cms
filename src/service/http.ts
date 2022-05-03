ee39195fa3cdd87e9e07f1c1e2fc70269bdcb78b
fangkai <1209683835@qq.com>
2022-05-03 23:20:10 +0800

    feat: 登录

--- src/service/http.ts
+++ src/service/http.ts
@@ -34,11 +34,11 @@ export const http = async (
   return window.fetch(`${apiUrl}${endpoint}`, config).then(async (response) => {
     if (response.ok) {
       const result = await response.json();
-      // if (result.code === "420") {
-      //   await auth.logout();
-      //   window.location.reload();
-      //   return Promise.reject({ message: "请重新登录" });
-      // }
+      if (result.code === 401) {
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({ message: "请重新登录" });
+      }
       if ([200, 201, 204].includes(result.code)) return result.data;
       else return Promise.reject(result);
     } else return Promise.reject({ message: response.statusText });
