2dfa689cc712ff7a27d619228d49647961b1e4cc
fangkai <1209683835@qq.com>
2022-05-05 14:40:53 +0800

    feat: 代理商

--- src/screen/authenticated-app/agents/components/agent-modal.tsx
+++ src/screen/authenticated-app/agents/components/agent-modal.tsx
@@ -1,27 +1,39 @@
-import { Form, Input, Modal } from "antd";
+import {
+  Button,
+  Col,
+  DatePicker,
+  Drawer,
+  Form,
+  Input,
+  Row,
+  Select,
+  Space,
+} from "antd";
+import { useAgentModal, useAgentsQueryKey } from "../util";
 import { useForm } from "antd/lib/form/Form";
 import { ErrorBox } from "components/lib";
-import { useAddAgent, useEditAgent } from "service/agent";
 import { Agent } from "types/agent";
+import { useAddAgent, useEditAgent } from "service/agent";
 import useDeepCompareEffect from "use-deep-compare-effect";
 import { cleanObject } from "utils";
-import { useAgentsQueryKey, useAgentModal } from "../util";
 
 export const AgentModal = ({ agents }: { agents: Agent[] }) => {
   const [form] = useForm();
+
   const { agentModalOpen, editingAgentId, close } = useAgentModal();
   const agent =
     agents?.find((item) => item.id === Number(editingAgentId)) || undefined;
+
   const useMutationAgent = editingAgentId ? useEditAgent : useAddAgent;
-  const { mutateAsync, isLoading, error } = useMutationAgent(
+  const { mutateAsync, error, isLoading } = useMutationAgent(
     useAgentsQueryKey()
   );
 
-  useDeepCompareEffect(() => {
-    if (agent) form.setFieldsValue(agent);
-  }, [agent, form]);
-
-  const confirm = () => {
+  const closeModal = () => {
+    form.resetFields();
+    close();
+  };
+  const submit = () => {
     form.validateFields().then(async () => {
       await mutateAsync(
         cleanObject({
@@ -33,36 +45,77 @@ export const AgentModal = ({ agents }: { agents: Agent[] }) => {
     });
   };
 
-  const closeModal = () => {
-    form.resetFields();
-    close();
-  };
+  useDeepCompareEffect(() => {
+    agent && form.setFieldsValue(agent);
+  }, [form, agent]);
 
   return (
-    <Modal
-      title={editingAgentId ? "编辑供应商信息" : "添加供应商"}
+    <Drawer
+      title={editingAgentId ? "编辑代理商信息" : "新增代理商"}
+      size={"large"}
+      forceRender={true}
+      onClose={closeModal}
       visible={agentModalOpen}
-      confirmLoading={isLoading}
-      onOk={confirm}
-      onCancel={closeModal}
+      bodyStyle={{ paddingBottom: 80 }}
+      extra={
+        <Space>
+          <Button onClick={closeModal}>取消</Button>
+          <Button onClick={submit} loading={isLoading} type="primary">
+            提交
+          </Button>
+        </Space>
+      }
     >
-      <ErrorBox error={error} />
       <Form form={form} layout="vertical">
-        <Form.Item
-          name="name"
-          label="供应商公司名称"
-          rules={[{ required: true, message: "请输入公司名称" }]}
-        >
-          <Input placeholder="请输入公司名称" />
-        </Form.Item>
-        <Form.Item
-          name="phone"
-          label="联系电话"
-          rules={[{ required: true, message: "请输入联系电话" }]}
-        >
-          <Input placeholder="请输入联系电话" />
-        </Form.Item>
+        <ErrorBox error={error} />
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="title"
+              label="头图标题"
+              rules={[{ required: true, message: "请输入头图标题" }]}
+            >
+              <Input placeholder="请输入头图标题" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item name="sort" label="头图排序">
+              <Input placeholder="请输入头图序号" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="dateRange"
+              label="投放时间"
+              rules={[{ required: true, message: "请选择投放时间" }]}
+            >
+              <DatePicker.RangePicker style={{ width: "100%" }} />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="is_show"
+              label="是否展示"
+              rules={[{ required: true, message: "请选择展示或隐藏" }]}
+            >
+              <Select placeholder="请选择展示或隐藏">
+                {[
+                  { name: "展示", value: "1" },
+                  { name: "隐藏", value: "0" },
+                ].map((item, index) => (
+                  <Select.Option key={index} value={item.value}>
+                    {item.name}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
       </Form>
-    </Modal>
+    </Drawer>
   );
 };
