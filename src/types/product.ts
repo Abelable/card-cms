441d58ea793e5310bd311f31e625279f5ea7267a
fangkai <1209683835@qq.com>
2022-05-06 11:21:44 +0800

    feat: 产品管理中心

--- src/types/product.ts
+++ src/types/product.ts
@@ -25,3 +25,24 @@ export interface ChannelsResult {
   data: Channel[];
   meta: { pagination: Pagination };
 }
+
+export interface ChannelForm {
+  operator_id: number;
+  supplier_id: number;
+  goods_name: string;
+  goods_code: string;
+  ownership: string[];
+  need_id_number: boolean;
+  need_id_card_pic: boolean;
+  deliver_area_type: number;
+  deliver_area: string[][];
+  age_limit: number[];
+  card_number_limit: number;
+  card_test_cycle: number;
+  risk_warning_type: number;
+  phone_repeat_limit: number;
+  phone_test_cycle: number;
+  address_repeat_limit: number;
+  address_test_cycle: number;
+  enable_automatic_blacklist_filtering: boolean;
+}
