e2c1fd1e038f45da4e0d648ee73dc9a55eb376de
fangkai <1209683835@qq.com>
2022-05-08 18:26:29 +0800

    feat: 批量导入

--- src/screen/authenticated-app/order-imports/index.tsx
+++ src/screen/authenticated-app/order-imports/index.tsx
@@ -18,18 +18,30 @@ export const OrderImports = () => {
         <Wrap gap>
           <FileUpload name="导入订单" />
           <Button icon={<DownloadOutlined />}>下载订单模版</Button>
-          <Divider style={{ height: "3rem" }} type={"vertical"} />
+          <Divider
+            style={{ height: "3rem", marginRight: "2.8rem" }}
+            type={"vertical"}
+          />
           <FileUpload name="上传照片" />
           <Button icon={<DownloadOutlined />}>下载照片模版</Button>
           <Tooltip title="照片包上传后识别匹配大概需要等待30分钟">
             <QuestionCircleOutlined style={{ cursor: "pointer" }} />
           </Tooltip>
-          <Divider style={{ height: "3rem" }} type={"vertical"} />
+          <Divider
+            style={{ height: "3rem", marginRight: "2.8rem" }}
+            type={"vertical"}
+          />
           <FileUpload name="拼多多证件导入" />
           <Button icon={<DownloadOutlined />}>下载拼多多模版</Button>
-          <Divider style={{ height: "3rem" }} type={"vertical"} />
+          <Divider
+            style={{ height: "3rem", marginRight: "2.8rem" }}
+            type={"vertical"}
+          />
           <FileUpload name="导入天猫信息" />
-          <Divider style={{ height: "3rem" }} type={"vertical"} />
+          <Divider
+            style={{ height: "3rem", marginRight: "2.8rem" }}
+            type={"vertical"}
+          />
           <FileUpload name="导入有赞信息" />
         </Wrap>
         <List
