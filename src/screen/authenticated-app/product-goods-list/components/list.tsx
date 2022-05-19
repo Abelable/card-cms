555b2e12b01b3e08cd64e8a2ad1745116b39fb5c
fangkai <1209683835@qq.com>
2022-05-19 14:08:25 +0800

    feat: 在售商品管理

--- src/screen/authenticated-app/product-goods-list/components/list.tsx
+++ src/screen/authenticated-app/product-goods-list/components/list.tsx
@@ -18,6 +18,7 @@ import {
   usePublishModal,
 } from "../util";
 import { useNavigate } from "react-router";
+import { ColumnsType } from "antd/lib/table";
 
 interface ListProps extends TableProps<Goods>, SearchPanelProps {
   error: Error | unknown;
@@ -47,6 +48,128 @@ export const List = ({
       per_page: pagination.pageSize,
     });
 
+  const columns: ColumnsType<Goods> = [
+    {
+      title: "编号",
+      dataIndex: "id",
+      width: "8rem",
+      sorter: (a, b) => Number(a.id) - Number(b.id),
+    },
+    {
+      title: "商品名称",
+      render: (value, goods) => (
+        <div style={{ display: "flex" }}>
+          <Image width={90} height={90} src={goods.main_picture} />
+          <GoodsInfoWrap>
+            <div>{goods.name}</div>
+            {goods.sale_point ? (
+              goods.sale_point
+                .split(",")
+                .map((item, index) => <Tag key={index}>{item}</Tag>)
+            ) : (
+              <></>
+            )}
+            <div style={{ color: "#999" }}>商品编码：{goods.encoding}</div>
+            <div style={{ color: "#999" }}>发布时间：{goods.created_at}</div>
+          </GoodsInfoWrap>
+        </div>
+      ),
+    },
+    {
+      title: "供应商&产品",
+      render: (value, goods) => (
+        <>
+          <div>
+            供应商：
+            {
+              supplierOptions.find(
+                (item) => item.id === goods.product.supplier_id
+              )?.name
+            }
+          </div>
+          <div>产品：{goods.product.name}</div>
+        </>
+      ),
+    },
+    {
+      title: "代理商",
+      render: (value, goods) => (
+        <>
+          <div>
+            <ButtonNoPadding
+              type={"link"}
+              onClick={() => editAgent(String(goods.id))}
+            >
+              设置代理商可见
+            </ButtonNoPadding>
+          </div>
+          <div>
+            <ButtonNoPadding
+              type={"link"}
+              onClick={() => linkToAgents(goods.id)}
+            >
+              查看代理商
+            </ButtonNoPadding>
+          </div>
+        </>
+      ),
+    },
+  ];
+
+  const editColumns: ColumnsType<Goods> = [
+    {
+      title: "编辑商品",
+      render: (value, goods) => (
+        <>
+          <div>
+            <ButtonNoPadding
+              type={"link"}
+              onClick={() => linkToChannels(goods.product_id)}
+            >
+              修改产品信息
+            </ButtonNoPadding>
+          </div>
+          <div>
+            <ButtonNoPadding
+              type={"link"}
+              onClick={() => startEdit(String(goods.id))}
+            >
+              修改商品信息
+            </ButtonNoPadding>
+          </div>
+          {params.is_removed === "0" ? (
+            <div>
+              <ButtonNoPadding type={"link"}>下架</ButtonNoPadding>
+            </div>
+          ) : (
+            <>
+              <div>
+                <Button type={"link"}>上架</Button>
+              </div>
+              <div>
+                <Button type={"link"}>删除</Button>
+              </div>
+            </>
+          )}
+        </>
+      ),
+    },
+  ];
+
+  const linkColumns: ColumnsType<Goods> = [
+    {
+      title: "操作",
+      render: (value, goods) => (
+        <ButtonNoPadding
+          type={"link"}
+          onClick={() => checkLink(String(goods.id))}
+        >
+          推广链接
+        </ButtonNoPadding>
+      ),
+    },
+  ];
+
   return (
     <Container>
       <Header between={true}>
@@ -69,114 +192,11 @@ export const List = ({
       <ErrorBox error={error} />
       <Table
         rowKey={"id"}
-        columns={[
-          {
-            title: "编号",
-            dataIndex: "id",
-            width: "8rem",
-            sorter: (a, b) => Number(a.id) - Number(b.id),
-          },
-          {
-            title: "商品名称",
-            render: (value, goods) => (
-              <div style={{ display: "flex" }}>
-                <Image width={90} height={90} src={goods.main_picture} />
-                <GoodsInfoWrap>
-                  <div>{goods.name}</div>
-                  {goods.sale_point ? (
-                    goods.sale_point
-                      .split(",")
-                      .map((item, index) => <Tag key={index}>{item}</Tag>)
-                  ) : (
-                    <></>
-                  )}
-                  <div style={{ color: "#999" }}>
-                    商品编码：{goods.encoding}
-                  </div>
-                  <div style={{ color: "#999" }}>
-                    发布时间：{goods.created_at}
-                  </div>
-                </GoodsInfoWrap>
-              </div>
-            ),
-          },
-          {
-            title: "供应商&产品",
-            render: (value, goods) => (
-              <>
-                <div>
-                  供应商：
-                  {
-                    supplierOptions.find(
-                      (item) => item.id === goods.product.supplier_id
-                    )?.name
-                  }
-                </div>
-                <div>产品：{goods.product.name}</div>
-              </>
-            ),
-          },
-          {
-            title: "代理商",
-            render: (value, goods) => (
-              <>
-                <div>
-                  <ButtonNoPadding
-                    type={"link"}
-                    onClick={() => editAgent(String(goods.id))}
-                  >
-                    设置代理商可见
-                  </ButtonNoPadding>
-                </div>
-                <div>
-                  <ButtonNoPadding
-                    type={"link"}
-                    onClick={() => linkToAgents(goods.id)}
-                  >
-                    查看代理商
-                  </ButtonNoPadding>
-                </div>
-              </>
-            ),
-          },
-          {
-            title: "编辑商品",
-            render: (value, goods) => (
-              <>
-                <div>
-                  <ButtonNoPadding
-                    type={"link"}
-                    onClick={() => linkToChannels(goods.product_id)}
-                  >
-                    修改产品信息
-                  </ButtonNoPadding>
-                </div>
-                <div>
-                  <ButtonNoPadding
-                    type={"link"}
-                    onClick={() => startEdit(String(goods.id))}
-                  >
-                    修改商品信息
-                  </ButtonNoPadding>
-                </div>
-                <div>
-                  <ButtonNoPadding type={"link"}>下架</ButtonNoPadding>
-                </div>
-              </>
-            ),
-          },
-          {
-            title: "操作",
-            render: (value, goods) => (
-              <ButtonNoPadding
-                type={"link"}
-                onClick={() => checkLink(String(goods.id))}
-              >
-                推广链接
-              </ButtonNoPadding>
-            ),
-          },
-        ]}
+        columns={
+          params.is_removed === "0"
+            ? [...columns, ...editColumns, ...linkColumns]
+            : [...columns, ...editColumns]
+        }
         onChange={setPagination}
         {...restProps}
       />
