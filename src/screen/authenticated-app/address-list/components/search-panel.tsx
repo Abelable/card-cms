6945b91e4b23577b9282cd9683b396ecb798d929
Administrator <admin@example.com>
2023-04-25 18:22:05 +0800

    feat: 地址列表

---
+++ src/screen/authenticated-app/address-list/components/search-panel.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { Button, Select } from "antd";
+import { Row } from "components/lib";
+import styled from "@emotion/styled";
+import type { ProductsSearchParams } from "types/order";
+import type { SupplierOption } from "types/supplier";
+
+export interface SearchPanelProps {
+  supplierOptions: SupplierOption[];
+  params: Partial<ProductsSearchParams>;
+  setParams: (params: Partial<ProductsSearchParams>) => void;
+}
+
+export const SearchPanel = ({
+  supplierOptions,
+  params,
+  setParams,
+}: SearchPanelProps) => {
+  const defaultParams = {
+    supplier_id: undefined,
+  } as Partial<ProductsSearchParams>;
+
+  const [temporaryParams, setTemporaryParams] =
+    useState<Partial<ProductsSearchParams>>(params);
+
+  const setSupplier = (supplier_id: number) =>
+    setTemporaryParams({ ...temporaryParams, supplier_id });
+  const clearSupplier = () =>
+    setTemporaryParams({ ...temporaryParams, supplier_id: undefined });
+
+  const clear = () => {
+    setParams({ ...params, ...defaultParams });
+    setTemporaryParams({ ...temporaryParams, ...defaultParams });
+  };
+
+  return (
+    <Container marginBottom={1.6} between={true}>
+      <Row gap={true}>
+        <Row>
+          <div>供应商：</div>
+          <Select
+            style={{ width: "20rem" }}
+            value={temporaryParams.supplier_id}
+            allowClear={true}
+            onSelect={setSupplier}
+            onClear={clearSupplier}
+            showSearch
+            filterOption={(input, option) =>
+              (option!.children as unknown as string)
+                .toLowerCase()
+                .includes(input.toLowerCase())
+            }
+            placeholder="请选择供应商"
+          >
+            {supplierOptions.map(({ id, name }) => (
+              <Select.Option key={id} value={id}>
+                {name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Row>
+      </Row>
+      <Row gap={true}>
+        <Button onClick={clear}>重置</Button>
+        <Button
+          style={{ marginRight: 0 }}
+          type={"primary"}
+          onClick={() => setParams({ ...params, ...temporaryParams })}
+        >
+          查询
+        </Button>
+      </Row>
+    </Container>
+  );
+};
+
+const Container = styled(Row)`
+  padding: 2.4rem;
+  background: #fff;
+`;
---
+++ src/screen/authenticated-app/address-list/util.ts
@@ -0,0 +1,57 @@
+import { useSetUrlSearchParams, useUrlQueryParams } from "utils/url";
+import { useCallback, useMemo } from "react";
+
+export const useProductsSearchParams = () => {
+  const [params, setParams] = useUrlQueryParams([
+    "supplier_id",
+    "page",
+    "per_page",
+  ]);
+  return [
+    useMemo(
+      () => ({
+        page: Number(params.page) || 1,
+        per_page: Number(params.per_page) || 10,
+        ...params,
+      }),
+      [params]
+    ),
+    setParams,
+  ] as const;
+};
+
+export const useProductsQueryKey = () => {
+  const [params] = useProductsSearchParams();
+  return ["products", params];
+};
+
+export const useProductModal = () => {
+  const [{ productCreate }, setProductModalOpen] = useUrlQueryParams([
+    "productCreate",
+  ]);
+  const [{ editingProductId }, setEditingProductId] = useUrlQueryParams([
+    "editingProductId",
+  ]);
+  const setUrlParams = useSetUrlSearchParams();
+
+  const open = useCallback(
+    () => setProductModalOpen({ productCreate: true }),
+    [setProductModalOpen]
+  );
+  const startEdit = useCallback(
+    (id: string) => setEditingProductId({ editingProductId: id }),
+    [setEditingProductId]
+  );
+  const close = useCallback(
+    () => setUrlParams({ productCreate: "", editingProductId: "" }),
+    [setUrlParams]
+  );
+
+  return {
+    productModalOpen: productCreate === "true" || !!editingProductId,
+    editingProductId,
+    open,
+    startEdit,
+    close,
+  };
+};
